3df170214176f7bd75fee1c63dfd0fd696752006
Dylan Arrabito <djarrabito@gmail.com>
2019-06-13 10:40:53 -0400

    Modified slaveinfo for testing

--- test/linux/slaveinfo/slaveinfo.c
+++ test/linux/slaveinfo/slaveinfo.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 #include "ethercat.h"
 
@@ -505,6 +506,146 @@ void si_sdo(int cnt)
     }
 }
 
+
+static int el2535_writeu8 (uint16 slave, uint16 index, uint8 subindex, uint8 value)
+{
+    int wkc;
+
+    wkc = ec_SDOwrite (slave, index, subindex, FALSE, sizeof(value), &value,
+                       EC_TIMEOUTRXM);
+    return wkc;
+}
+
+static int el2535_writeu16 (uint16 slave, uint16 index, uint8 subindex, uint16 value)
+{
+    int wkc;
+
+    wkc = ec_SDOwrite (slave, index, subindex, FALSE, sizeof(value), &value,
+                       EC_TIMEOUTRXM);
+    return wkc;
+}
+
+static int el2535_write16 (uint16 slave, uint16 index, uint8 subindex, int16 value)
+{
+    int wkc;
+
+    wkc = ec_SDOwrite (slave, index, subindex, FALSE, sizeof(value), &value,
+                       EC_TIMEOUTRXM);
+    return wkc;
+}
+
+//static int el2535_writeu32 (uint16 slave, uint16 index, uint8 subindex, uint32 value)
+//{
+//    int wkc;
+//
+//    wkc = ec_SDOwrite (slave, index, subindex, FALSE, sizeof(value), &value,
+//                       EC_TIMEOUTRXM);
+//    return wkc;
+//}
+
+static int el2535_write32 (uint16 slave, uint16 index, uint8 subindex, int32 value)
+{
+    int wkc;
+
+    wkc = ec_SDOwrite (slave, index, subindex, FALSE, sizeof(value), &value,
+                       EC_TIMEOUTRXM);
+    return wkc;
+}
+
+static int el2535_setup(uint16 slave)
+{
+    printf ("el2535 drive setup\n");
+
+    el2535_writeu8  (slave, 0x1C13, 0x00, 0);
+    el2535_write16  (slave, 0x1C13, 0x01, 0x1a01);
+    //el2535_write16  (slave, 0x1C13, 0x02, 0x1a01);
+    //el2535_write16  (slave, 0x1C13, 0x03, 0x1a02);
+
+    el2535_writeu8  (slave, 0x1C13, 0x00, 1);
+
+    el2535_writeu8  (slave, 0x8000, 0x03, 0);     // en dithering
+    el2535_writeu8  (slave, 0x8000, 0x04, 0);     // invert polarity
+    el2535_writeu8  (slave, 0x8000, 0x05, 1);     // watchdog
+
+    el2535_write16  (slave, 0x8000, 0x0B, 0);     // offset
+    el2535_write32  (slave, 0x8000, 0x0C, 65536); // gain
+
+    el2535_write16  (slave, 0x8000, 0x0D, 0);     // output in case of error
+    el2535_writeu16 (slave, 0x8000, 0x0E, 65535); // output ramp
+
+    el2535_writeu8  (slave, 0x8000, 0x10, 10);    // current limit %
+    el2535_writeu16 (slave, 0x8000, 0x12, 350);   // Kp
+    el2535_writeu16 (slave, 0x8000, 0x13, 6);     // Ki
+    el2535_writeu16 (slave, 0x8000, 0x14, 50);    // Kd
+
+    el2535_writeu16 (slave, 0x8000, 0x1E, 100);   // dither freq
+    el2535_writeu8  (slave, 0x8000, 0x1F, 10);    // dither amp
+
+    el2535_writeu8  (slave, 0x8000, 0x21, 0);    // info data
+    el2535_writeu8  (slave, 0x8000, 0x22, 1);    // info data
+
+
+    el2535_writeu8  (slave, 0x8010, 0x03, 0);     // en dithering
+    el2535_writeu8  (slave, 0x8010, 0x04, 0);     // invert polarity
+    el2535_writeu8  (slave, 0x8010, 0x05, 0);     // watchdog
+
+    el2535_write16  (slave, 0x8010, 0x0B, 0);     // offset
+    el2535_write32  (slave, 0x8010, 0x0C, 65536); // gain
+
+    el2535_write16  (slave, 0x8010, 0x0D, 0);     // output in case of error
+    el2535_writeu16 (slave, 0x8010, 0x0E, 65535); // output ramp
+
+    el2535_writeu8  (slave, 0x8010, 0x10, 90);    // current limit %
+    el2535_writeu16 (slave, 0x8010, 0x12, 350);   // Kp
+    el2535_writeu16 (slave, 0x8010, 0x13, 6);     // Ki
+    el2535_writeu16 (slave, 0x8010, 0x14, 50);    // Kd
+
+    el2535_writeu16 (slave, 0x8010, 0x1E, 100);   // dither freq
+    el2535_writeu8  (slave, 0x8010, 0x1F, 10);    // dither amp
+
+
+    return 0;
+}
+
+typedef struct
+{
+    bool    en_dithering    :1;
+    int64_t                 :4;
+    bool    en_cc           :1;
+    bool    reset           :1;
+    int64_t                 :9;
+    int16_t pwm_out         :16;
+    bool    en_dithering2   :1;
+    int64_t                 :4;
+    bool    en_cc2          :1;
+    bool    reset2          :1;
+    int64_t                 :9;
+    int16_t pwm_out2        :16;
+} out_EL2535t;
+
+out_EL2535t *out_el2535_1;
+
+//typedef struct
+//{
+//    bool    dig_in1          :1;
+//    int64_t                  :3;
+//    bool    ready_en         :1;
+//    bool    warning          :1;
+//    bool    error            :1;
+//    int64_t                  :8;
+//    bool    txpdo_toggle     :1;
+//    int16_t info_data_1      :16;
+//    int16_t info_data_2      :16;
+//} in_EL2535t;
+
+typedef struct
+{
+    int16_t info_data_1      :16;
+    int16_t info_data_2      :16;
+} in_EL2535t;
+
+in_EL2535t *in_el2535_1;
+
 void slaveinfo(char *ifname)
 {
    int cnt, i, j, nSM;
@@ -518,10 +659,47 @@ void slaveinfo(char *ifname)
    {
       printf("ec_init on %s succeeded.\n",ifname);
       /* find and auto-config slaves */
-      if ( ec_config(FALSE, &IOmap) > 0 )
+      if ( ec_config_init(FALSE) > 0 )
       {
-         ec_configdc();
-         while(EcatError) printf("%s", ec_elist2string());
+          if (ec_slavecount > 0)
+          {
+              ec_slavet * slave = &ec_slave[2];
+
+              // el2535 drive
+              if(slave->eep_man == 2 && slave->eep_id == 0x09e73052)
+              {
+                  printf ("Found el2535 drive\n");
+                  slave->PO2SOconfig = el2535_setup;
+              }
+          }
+
+          ec_slave[10].CoEdetails &= ~ECT_COEDET_SDOCA;
+          ec_slave[11].CoEdetails &= ~ECT_COEDET_SDOCA;
+          ec_slave[2].CoEdetails &= ~ECT_COEDET_SDOCA;
+
+
+          ec_config_map(&IOmap);
+          ec_configdc();
+
+
+          out_el2535_1 = (out_EL2535t*) ec_slave[2].outputs;
+          out_el2535_1->en_dithering = 1;
+          out_el2535_1->en_cc = 1;
+          out_el2535_1->en_dithering2 = 0;
+          out_el2535_1->en_cc2 = 0;
+
+          in_el2535_1 = (in_EL2535t*) ec_slave[2].inputs;
+
+          printf("in 1: %X\r\n", ec_slave[2].inputs[0]);
+          printf("in 2: %X\r\n", ec_slave[2].inputs[1]);
+          printf("in 3: %X\r\n", ec_slave[2].inputs[2]);
+          printf("in 4: %X\r\n", ec_slave[2].inputs[3]);
+
+          printf("info 1: %X\r\n", in_el2535_1->info_data_1);
+          printf("info 2: %X\r\n", in_el2535_1->info_data_2);
+
+
+          while(EcatError) printf("%s", ec_elist2string());
          printf("%d slaves found and configured.\n",ec_slavecount);
          expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
          printf("Calculated workcounter %d\n", expectedWKC);
@@ -541,6 +719,26 @@ void slaveinfo(char *ifname)
             }
          }
 
+          printf("Request operational state for all slaves\n");
+          ec_slave[0].state = EC_STATE_OPERATIONAL;
+          /* send one valid process data to make outputs in slaves happy*/
+          ec_send_processdata();
+          ec_receive_processdata(EC_TIMEOUTRET);
+          /* request OP state for all slaves */
+          ec_writestate(0);
+          int chk = 40;
+          /* wait for all slaves to reach OP state */
+          do
+          {
+              ec_send_processdata();
+              ec_receive_processdata(EC_TIMEOUTRET);
+              ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
+          }
+          while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL));
+          if (ec_slave[0].state == EC_STATE_OPERATIONAL )
+          {
+              printf("Operational state reached for all slaves.\n");
+          }
 
          ec_readstate();
          for( cnt = 1 ; cnt <= ec_slavecount ; cnt++)
